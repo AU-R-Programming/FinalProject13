7d8f86f3d05da39cf873cc2701a03d046426f781
sbs0068 <sbs0068@auburn.edu>
2024-12-04 20:43:36 -0600

    Update Initial_Values.r

--- FinalProjectGroup13/R/Initial_Values.r
+++ FinalProjectGroup13/R/Initial_Values.r
@@ -1,11 +1,11 @@
 #' @title Estimate Beta Coefficients
-#' 
+#'
 #' @description
 #' estimates the coefficient vector βwhich includes the independent variables/predictors plus the intercept
-#' 
+#'
 #' @param X A matrix of predictor variables (features)
 #' @param y A vector of binary response variables (0 or )1
-#' 
+#'
 #' @return A list containing:
 #'   \item{coefficients}{The estimated beta coefficients}
 #'   \item{fitted_values}{The predicted probabilities for the training data}
@@ -21,7 +21,7 @@
 #' X <- matrix(rnorm(100 * 2), ncol = 2)
 #' y <- rbinom(100, 1, 0.5)
 #' model <- estimate_beta(X, y)
-#' 
+#'
 #' @export
 estimate_beta <- function(X, y) {
     # Add intercept term column
@@ -33,7 +33,7 @@ estimate_beta <- function(X, y) {
         #calculate probabilities
         p <- 1 / (1 + exp(-X %*% beta))
         # make sure probabilites are between 0 and 1 so log works as intended
-        p <- pmax(pmin(p, 0.9999), 0.0001) 
+        p <- pmax(pmin(p, 0.9999), 0.0001)
         # Return result of loss function
         -sum(y * log(p) + (1 - y) * log(1 - p))
     }
@@ -58,16 +58,16 @@ estimate_beta <- function(X, y) {
 }
 
 #' @title Predict Probabilities for Logistic Regression
-#' 
+#'
 #' @description
 #' Calculates prediction probabilities for new data using estimated beta coefficients
 #' from a logistic regression model.
-#' 
+#'
 #' @param X A matrix of predictor variables (features) for new data
 #' @param beta A vector of estimated beta coefficients (including intercept)
-#' 
+#'
 #' @return A vector of predicted probabilities between 0 and 1
-#' 
+#'
 #' @examples
 #' X_new <- matrix(rnorm(50 * 2), ncol = 2)
 #' predictions <- predict_prob(X_new, model$coefficients)
@@ -78,6 +78,7 @@ predict_prob <- function(X, beta) {
     X <- cbind(1, X)
     1 / (1 + exp(-X %*% beta))
 }
+
 
 ## Sources
 #https://www.rdocumentation.org/packages/base/versions/3.6.2/topics/solve
